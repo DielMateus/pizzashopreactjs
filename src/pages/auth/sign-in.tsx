6b5f6bc8e983d30f554943ec1c72ca2c93bc55be
Jenkins <dielmp91@gmail.com>
2025-02-18 07:39:20 -0300

    Subindo projeto até a aula Interface otimista

---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,79 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@radix-ui/react-label"
+import { Helmet } from "react-helmet-async"
+import { useForm } from "react-hook-form"
+import { Link, useSearchParams  } from 'react-router-dom'
+import { z } from 'zod'
+import { toast } from "sonner"
+import { useMutation } from "@tanstack/react-query"
+import { signIn } from '@/api/sign-in'
+
+const signInForm = z.object({
+    email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
+export function SignIn() {
+    const [searchParams] = useSearchParams()
+    const { register, handleSubmit, formState: { isSubmitting } 
+    } = useForm<SignInForm>({
+        defaultValues: {
+            // E aqui se eu tiver o e-mail cadastrado já aparece para logar senão vazio
+          email: searchParams.get('email') ?? '',
+        },
+      })
+
+    // mutateAsync--> foi renomeada para authenticate
+    const { mutateAsync: authenticate } = useMutation({
+        mutationFn: signIn,
+      })
+
+    async function handleSignIn(data: SignInForm) {
+        try{
+            console.log(data)
+
+            await authenticate({ email: data.email })
+    
+            toast.success('Enviamos um link de autenticação para seu e-mail.', {
+                action: {
+                    label: 'Reenviar',
+                    onClick: () => handleSignIn(data),
+                }
+            })
+        } catch{
+            toast.error('Credenciais inválidas.');
+        }
+        
+    }
+
+    return <> 
+    <Helmet title="Login" /> 
+    <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+            <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+            <div className="flex flex-col gap-2 text-center">
+                <h1 className="text-2xl font-semibold tracking-tight">
+                    Acessa painel
+                </h1>
+                <p className="text-sm text-muted-foreground">
+                    Acompanhe suas vendas pelo painel do parceiro!
+                </p>
+            </div>
+            <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+                <div className="space-y-2">
+                    <Label htmlFor="email">Seu e-mail</Label>
+                    <Input id="email" type="email" {...register('email')}/>
+                </div>
+
+                <Button disabled={isSubmitting} className="w-full" type="submit">
+                    Acessar painel
+                </Button>
+            </form>
+        </div>
+    </div>
+    </>
+}
