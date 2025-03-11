9abc12541ffdc6b60fc7f490d207680768c9c0e0
Jenkins <dielmp91@gmail.com>
2025-03-11 09:35:36 -0300

    Config Setup do ambiente de testes

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,41 @@
 import path from "node:path";
 
 import react from "@vitejs/plugin-react";
-import type { UserConfig } from "vite";
-import { defineConfig } from "vite";
-import { InlineConfig } from "vite/node";
-// https://vitejs.dev/config/
+import { defineConfig, UserConfig } from "vite";
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "a-vitest-plugin-that-changes-config",
+      config: () => ({
+        test: {
+          setupFiles: [
+            "./setupFiles/add-something-to-global.ts",
+            "setupFiles/without-relative-path-prefix.ts",
+          ],
+        },
+      }),
+    },
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   test: {
+    globalSetup: [
+      "./globalSetup/default-export.js",
+      "./globalSetup/named-exports.js",
+      "./globalSetup/ts-with-imports.ts",
+      "./globalSetup/another-vite-instance.ts",
+    ],
     globals: true,
-    // setupFiles: ['./test/setup.ts'],
-    // environment: 'happy-dom',
+    setupFiles: ["./test/setup.ts"],
+    environment: "happy-dom",
   },
 } as UserConfig & {
-  test: InlineConfig;
+  test: {
+    globals: true;
+  };
 });
-
-// import path from "node:path";
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// /// <reference types="vitest/config" />
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// });
