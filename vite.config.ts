598b8c73adbbe882ff310af3aca8e252c78ab05f
Jenkins <dielmp91@gmail.com>
2025-03-12 10:25:42 -0300

    Correção das dependências e bibliotecas que o arquivo package.json não estava atualizando, mesmo rodando as instalações: Solução foi inserir manualmente as dependências, exluir node_modules e rodar novamente os testes: npm run test.

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,22 @@
 import path from "node:path";
 
 import react from "@vitejs/plugin-react";
-import { defineConfig, UserConfig } from "vite";
-
+import type { UserConfig } from "vite";
+import { defineConfig } from "vite";
+import type { InlineConfig } from "vitest/node";
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "a-vitest-plugin-that-changes-config",
-      config: () => ({
-        test: {
-          setupFiles: [
-            "./setupFiles/add-something-to-global.ts",
-            "setupFiles/without-relative-path-prefix.ts",
-          ],
-        },
-      }),
-    },
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   test: {
-    globalSetup: [
-      "./globalSetup/default-export.js",
-      "./globalSetup/named-exports.js",
-      "./globalSetup/ts-with-imports.ts",
-      "./globalSetup/another-vite-instance.ts",
-    ],
     globals: true,
     setupFiles: ["./test/setup.ts"],
     environment: "happy-dom",
   },
 } as UserConfig & {
-  test: {
-    globals: true;
-  };
+  test: InlineConfig;
 });
